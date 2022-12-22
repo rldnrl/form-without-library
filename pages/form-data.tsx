790aa6c2fb751dcb9fed3e7cd9a70e0f65014ffa
rldnrl <devpark1002@gmail.com>
2022-12-22 20:06:17 +0900

    feat: form data를 활용하는 예제

--- pages/form-data.tsx
+++ pages/form-data.tsx
@@ -1,9 +1,7 @@
-# Library 없이 Form 관리해보기
+import { FormEventHandler } from "react";
+import Input from "../src/components/Input";
 
-## 1. `FormData` 객체 이용하기
-
-```tsx
-function Home() {
+export default function () {
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     const target = e.target as HTMLFormElement;
@@ -29,8 +27,3 @@ function Home() {
     </div>
   );
 }
-```
-
-`form` 태그 하위에 있는 `input` 태그의 `name`과 `value`를 매핑시켜준다.
-
-## 2. `useState`에서 객체로 관리하기.

e7b55f1dcf3a808e62c5396d905b46155f2cf756
rldnrl <devpark1002@gmail.com>
2022-12-23 19:25:23 +0900

    feat: add errorMessage spec

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,8 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useRef, useState } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  errorMessage?: string;
 }
 
 export default function Input({
@@ -9,20 +10,37 @@ export default function Input({
   type,
   name,
   value,
+  pattern,
   placeholder,
+  errorMessage,
   onChange,
 }: InputProps) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [focused, setFocused] = useState<boolean>(false);
+
+  const handleBlur = () => {
+    if (inputRef.current) {
+      inputRef.current.dataset.focused = "true";
+    }
+    setFocused(true);
+  };
+
   return (
     <label className="flex flex-col">
       <span>{label}</span>
       <input
+        ref={inputRef}
         className="p-[15px] my-[10px] outline-gray-300 outline focus:outline-gray-700 rounded-md"
         type={type}
         name={name}
         value={value}
+        pattern={pattern}
         placeholder={placeholder}
+        onBlur={handleBlur}
         onChange={onChange}
+        data-focused={focused}
       />
+      <span className="input-error-message">{errorMessage}</span>
     </label>
   );
 }

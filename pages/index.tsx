c7e28a1fac641008249868d6e22a972edb6c31d1
rldnrl <devpark1002@gmail.com>
2022-12-23 19:24:31 +0900

    feat: add use state link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
-import { FormEventHandler } from "react";
-import Input from "../src/components/Input";
 import Link from "next/link";
 
 export default function Home() {
-  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    const target = e.target as HTMLFormElement;
-    const data = new FormData(target);
-
-    console.log(Object.fromEntries(data.entries()));
-  };
-
   return (
     <div className="h-screen flex justify-center items-center">
       <Link href="/form-data">FormData를 이용하는 방법</Link>
+      <Link href="/use-state">useState를 이용하는 방법</Link>
     </div>
   );
 }

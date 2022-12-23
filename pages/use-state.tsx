36d66701dec9fbac0983fcb0218fb962b1cf26c3
rldnrl <devpark1002@gmail.com>
2022-12-23 19:35:34 +0900

    feat: change first name to password

--- pages/use-state.tsx
+++ pages/use-state.tsx
@@ -31,21 +31,21 @@ export default function () {
       label: "Email",
     },
     {
-      name: "firstName",
-      type: "text",
-      placeholder: "First Name",
-      errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
-      label: "First Name",
-    },
-    {
       name: "password",
       type: "password",
       placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      label: "Password",
+    },
+    {
+      name: "confirm-password",
+      type: "password",
+      placeholder: "Confirm Password",
       errorMessage: "Passwords don't match!",
       pattern: formValues.password,
-      label: "Password",
+      label: "Confirm Password",
     },
   ];
 
